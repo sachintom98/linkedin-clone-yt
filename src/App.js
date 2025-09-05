016664727068fa2f0cf6a520d7e08422d9cee9c1
sachintom98 <sachintom98@gmail.com>
2025-09-05 12:50:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <h1>lets build a linkedin cole</h1> */}
+
+    <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+
+
+function Header() {
+  return (
+    <div className="header">
+      {/* <h1>This is the header</h1> */}
+
+      <div className="header__left">
+        <img src="/linkedin-logo.svg" alt="" />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
+        </div>
+      </div>
+
+      <div className="header__right">
+
+      </div>
+    </div>
+  );
+}
+
+export default Header
